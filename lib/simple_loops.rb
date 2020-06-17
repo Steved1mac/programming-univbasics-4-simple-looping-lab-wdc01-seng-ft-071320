87a727013c954e76ad6fd5936395ec33e1575c20
Steven McAllister <steved1mac@gmail.com>
2020-06-17 02:55:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
-# Write your methods here
+ def loop_message_five_times(message)
+   x = 0
+   while x < 6
+   puts (message) 
+   x+=1
+ end
+   end
+   
+def loop_message_n_times(message, integer)
+  n = 0 
+  while n < integer  
+  puts message 
+  n+=1 
+end
+  end
+def output_array(array)
+  r = 0 
+  while r < array.length 
+  puts array 
+  r+=1 
+end
+  end
+def return_string_array(array)
+  count = 0 
+  while count < array.length 
+  count +=1 
+end
+  end 
